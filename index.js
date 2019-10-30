d0210c7a04057950c74921a878d39bbd4167668f
W-46ec <30034417+W-46ec@users.noreply.github.com>
2019-10-31 07:05:53 +0800

    login

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-// var app = express();
+
+var app = express()
+
+// Import user defined modules
+var db = require('./tools/database')		// defined in "./tools/database.js"
+var account = require('./routes/account')	// defined in "./routes/account.js"
 
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/login'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+app.use('/', account)	// find details in ./routes/account.js
+
+// app.get('/', (req, res) => res.render('pages/login'))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
