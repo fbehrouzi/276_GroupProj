0db3358ba76fd8326f9d0bc77b9fe9962dba46a1
W-46ec <30034417+W-46ec@users.noreply.github.com>
2019-11-04 06:06:04 +0800

    .

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ var confirmPwd = document.getElementById('confirm-pwd-box')
 if (pwd && confirmPwd) {
 	confirmPwd.addEventListener('change', () => {
 		if (confirmPwd.value != pwd.value) {
-			confirmPwd.setCustomValidity("Password don't match")
+			confirmPwd.setCustomValidity("Passwords don't match")
 		} else {
 			confirmPwd.setCustomValidity('')
 		}
