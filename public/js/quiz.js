c0f9d99056219afbe2faa77ce1bf9807fe7c3a55
dhaliwalk <kevindhal@gmail.com>
2019-11-04 05:19:35 -0800

    all quizzes implemented

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -55,3 +55,61 @@ function geographyCheck() {
   }
   document.getElementById("coinResult").innerHTML = "You Earned "+a*5+" Coins &#128176;";
 }
+
+function historyCheck() {
+  var a = 0;
+  var q1 = document.getElementsByName('q1');
+  if (q1[1].checked) {
+    a++;
+  }
+  var q2 = document.getElementsByName('q2');
+  if (q2[0].checked) {
+    a++;
+  }
+  var q3 = document.getElementsByName('q3');
+  if (q3[3].checked) {
+    a++;
+  }
+  var q4 = document.getElementsByName('q4');
+  if (q4[2].checked) {
+    a++;
+  }
+  document.getElementById("res").innerHTML = a+"/4";
+  document.getElementById("submit").innerHTML = "No more submitting";
+  if ( a < 3 ){
+    document.getElementById("message").innerHTML = "Better Luck Next Time";
+  }
+  else {
+    document.getElementById("message").innerHTML = "Great Job!";
+  }
+  document.getElementById("coinResult").innerHTML = "You Earned "+a*5+" Coins &#128176;";
+}
+
+function scienceCheck() {
+  var a = 0;
+  var q1 = document.getElementsByName('q1');
+  if (q1[1].checked) {
+    a++;
+  }
+  var q2 = document.getElementsByName('q2');
+  if (q2[3].checked) {
+    a++;
+  }
+  var q3 = document.getElementsByName('q3');
+  if (q3[2].checked) {
+    a++;
+  }
+  var q4 = document.getElementsByName('q4');
+  if (q4[2].checked) {
+    a++;
+  }
+  document.getElementById("res").innerHTML = a+"/4";
+  document.getElementById("submit").innerHTML = "No more submitting";
+  if ( a < 3 ){
+    document.getElementById("message").innerHTML = "Better Luck Next Time";
+  }
+  else {
+    document.getElementById("message").innerHTML = "Great Job!";
+  }
+  document.getElementById("coinResult").innerHTML = "You Earned "+a*5+" Coins &#128176;";
+}
