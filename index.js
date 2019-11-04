d8f727dbed081a1c71951302bfe8ef9a25906c76
dhaliwalk <kevindhal@gmail.com>
2019-11-03 20:34:31 -0800

    .

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const express = require('express')
 const path = require('path')
+var cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+var app = express()
+
+// Import user defined modules
+var db = require('./tools/database')		// defined in "./tools/database.js"
+var account = require('./routes/account')	// defined in "./routes/account.js"
+
+
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => { res.redirect('/main') })	// Home page
+app.use('/', account)	// Process requests related to user account
+						// Find details in "./routes/account.js"
+
+/* Operations that require login should be processed after this line */
+
+app.get('/main', (req, res) => {
+	res.render('pages/main', {
+		'username': req.cookies['username']
+    
+	})
+})
+
+
+// 404 page
+app.use((req, res) => {
+	res.status(404).render('pages/message', {
+		'title': "Not found",
+		'msg': "Oops! Page not found"
+	})
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
