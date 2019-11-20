5a1e68763c5e36903f118ac233c76118f899d524
W-46ec <30034417+W-46ec@users.noreply.github.com>
2019-11-20 15:47:02 +0800

    chatroom - display username

--- index.js
+++ index.js
@@ -16,30 +16,6 @@ var io = require('socket.io')(http);
 var pool = require('./tools/database').pool		// defined in "./tools/database.js"
 var account = require('./routes/account')		// defined in "./routes/account.js"
 
-var users = 0;
-app.get('/chat', (req, res) => res.render('pages/chatroom.ejs'))
-io.on('connection', function(socket){
-	users++;
-	if(users == 1){
-		io.emit('chat message', users+' user in chatroom');
-	}
-	else {
-		io.emit('chat message', users+' users in chatroom');
-	}
-	socket.on('disconnect', function(){
-	users--;
-	if(users == 1){
-		io.emit('chat message', users+' user in chatroom');
-	}
-	else {
-		io.emit('chat message', users+' users in chatroom');
-	}});
-
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg); //get username from data base and put it here before mssg user+msg
-	});
-});
-
 var main = require('./routes/main')			// defined in "./routes/main.js"
 var quiz = require('./routes/quiz')			// defined in "./routes/quiz.js"
 var store = require('./routes/store')		// defined in "./routes/store.js"
@@ -52,32 +28,64 @@ app.use(express.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//app.use('/', quiz)	// testing quiz
-
 app.get('/', (req, res) => { res.redirect('/main') })	// Home page
 app.use('/', account)	// Process requests related to user account
 						// Find details in "./routes/account.js"
 
 /* Operations that require login should be processed after this line */
 
-app.get('/inventory', (req, res) => {
-	var username = req.cookies['username']
-	var getuseraccount = `SELECT * FROM user_account WHERE username=$1`
-	// console.log(getuseraccount);
-	pool.query(getuseraccount, [username], (error, result) => {
-		if (error)
-			res.end(error);
-		var results = {'rows': result.rows };
-		// console.log(results);
-		res.render('pages/inventory', results)
+app.use('/', main)		// Process requests related to main page
+						// Find details in "./routes/main.js"
+
+app.use('/', quiz)		// Process requests related to quiz
+						// Find details in "./routes/quiz.js"
+
+app.use('/', store)		// Process requests related to store
+						// Find details in "./routes/store.js"
+
+var users = 0;
+app.get('/chat', (req, res) => res.render('pages/chatroom.ejs'))
+io.on('connection', function(socket) {
+	users++;
+	if(users == 1) {
+		io.emit('chat message', users + ' user in chatroom');
+	} else {
+		io.emit('chat message', users + ' users in chatroom');
+	}
+	socket.on('disconnect', function() {
+		users--;
+		if(users == 1) {
+			io.emit('chat message', users + ' user in chatroom');
+		} else {
+			io.emit('chat message', users + ' users in chatroom');
+		}
+	});
+	socket.on('chat message', function(msg) {
+		let cookieString = socket.request.headers.cookie
+		let lst = cookieString.split(';')
+		let username = ''
+		lst.map((curr, idx) => {
+			if (curr.split('=')[0] == 'username') {
+				username = curr.split('=')[1]
+			}
+		})
+		msg = username + ': ' + msg
+		io.emit('chat message', msg)
 	});
 });
 
-
-app.use('/', main)			// main page
-app.use('/', quiz)			// quiz
-app.use('/', store)			// store
-
+app.get('/inventory', (req, res) => {
+	var username = req.cookies['username']
+	var getuseraccount = `SELECT * FROM user_account WHERE username=$1`
+	pool.query(getuseraccount, [username], (error, result) => {
+		if (error)
+			res.end(error);
+		var results = {
+			'rows': result.rows
+		};
+		res.render('pages/inventory', results)
+	});
+});
 
 // 404 page
 app.use((req, res) => {
@@ -87,7 +95,4 @@ app.use((req, res) => {
 	})
 })
 
-// http.listen(5000, function(){
-//   console.log('listening on 5000');
-// });
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
