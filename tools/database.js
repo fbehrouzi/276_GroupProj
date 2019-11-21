a30bdecb0260a1d5230c67dc968b53f457d0245f
W-46ec <30034417+W-46ec@users.noreply.github.com>
2019-11-22 06:55:22 +0800

    Small changes

--- tools/database.js
+++ tools/database.js
@@ -6,10 +6,6 @@
 const { Pool } = require('pg')
 var pool = new Pool({
 	connectionString: process.env.DATABASE_URL
-	
-	//Michael's local testing because I can't read my .env file
-	//connectionString:'postgres://myuser:password@localhost:5432/userdb'
-
 })
 
 // Export variable 'pool'
