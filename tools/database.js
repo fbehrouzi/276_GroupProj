5ef8990ed200187d912eeb9e2f66dc8e680705cb
David Huang <33307716+hwhuang27@users.noreply.github.com>
2019-11-30 17:24:10 -0800

    grow/harvest buttons for main page

--- tools/database.js
+++ tools/database.js
@@ -8,6 +8,14 @@ var pool = new Pool({
 	connectionString: process.env.DATABASE_URL
 })
 
+// var pool = new Pool ({
+// 	user: 'postgres',
+// 	host: 'localhost',
+// 	database: 'farmagatchi',
+// 	password: 'root',
+// 	port: 5432
+// })
+
 // Export variable 'pool'
 module.exports = {
 	pool: pool
