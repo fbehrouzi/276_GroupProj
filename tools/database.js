b76b817d36c897e33f0a98afb3dffe24750ca292
longkitl <long_kit_lam@sfu.ca>
2019-12-01 01:24:45 -0800

    forgot to change database url again

--- tools/database.js
+++ tools/database.js
@@ -5,8 +5,8 @@
 // Database
 const { Pool } = require('pg')
 var pool = new Pool({
-	connectionString:'postgres://myuser:password@localhost:5432/userdb'
-	//connectionString: process.env.DATABASE_URL
+	//connectionString:'postgres://myuser:password@localhost:5432/userdb'
+	connectionString: process.env.DATABASE_URL
 })
 
 // var pool = new Pool ({
