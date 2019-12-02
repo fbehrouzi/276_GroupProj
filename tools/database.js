5a5eb492acd74e91b32f50f58e2e36b489593e88
W-46ec <30034417+W-46ec@users.noreply.github.com>
2019-12-02 09:52:48 +0800

    bug fixed

--- tools/database.js
+++ tools/database.js
@@ -5,7 +5,6 @@
 // Database
 const { Pool } = require('pg')
 var pool = new Pool({
-	//connectionString:'postgres://myuser:password@localhost:5432/userdb'
 	connectionString: process.env.DATABASE_URL
 })
 
