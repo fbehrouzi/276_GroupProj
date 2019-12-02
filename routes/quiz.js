e8cfcb398ffb9b379ba837733998fda02417c14d
dhaliwalk <kevindhal@gmail.com>
2019-12-02 15:29:24 -0800

    adding new quizzes

--- routes/quiz.js
+++ routes/quiz.js
@@ -9,13 +9,16 @@ var pool = require('../tools/database').pool		// defined in "../tools/database.j
 app.get('/quiz', (req, res) => { res.render('pages/quiz') })
 app.get('/math', (req, res) => res.render('pages/math'))
 app.get('/math2', (req, res) => res.render('pages/math2'))
+app.get('/math3', (req, res) => res.render('pages/math3'))
 app.get('/history', (req, res) => res.render('pages/history'))
 app.get('/history2', (req, res) => res.render('pages/history2'))
+app.get('/history3', (req, res) => res.render('pages/history3'))
 app.get('/science', (req, res) => res.render('pages/science'))
 app.get('/science2', (req, res) => res.render('pages/science2'))
+app.get('/science3', (req, res) => res.render('pages/science3'))
 app.get('/geography', (req, res) => res.render('pages/geography'))
 app.get('/geography2', (req, res) => res.render('pages/geography2'))
-
+app.get('/geography3', (req, res) => res.render('pages/geography3'))
 // Path: "/checkin"
 // Method: POST
 // Desc: verify the quiz answer
@@ -33,18 +36,18 @@ app.post('/checkquiz', (req, res) => {
 	pool.query(query_cmd, [coins, username] , (err, results) => {
 		if (err) {
 			res.status(500).render('pages/message', {
-				'title': 'Error', 
+				'title': 'Error',
 				'msg': 'Database error'
 			})
 		} else {
 			if (coins === 0) {
 				res.status(200).render('pages/message', {
-					'title': 'Sorry, wrong answer~', 
+					'title': 'Sorry, wrong answer~',
 					'msg': `Better Luck Next Time`
 				})
 			} else {
 				res.status(200).render('pages/message', {
-					'title': `Great job!`, 
+					'title': `Great job!`,
 					'msg': `You correctly answered ${ parseInt(coins / 5) } question(s). Earned ${ coins } Coins`
 				})
 			}
@@ -55,4 +58,3 @@ app.post('/checkquiz', (req, res) => {
 module.exports = app	// Export app
 
 // End of quiz.js
-
